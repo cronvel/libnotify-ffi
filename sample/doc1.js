d1df8baa7fba3427719177fe5c69a54b56958235
Cédric Ronvel <cedric.ronvel@gmail.com>
2015-11-19 16:01:26 +0100

    doc1.js

--- sample/doc1.js
+++ sample/doc1.js
@@ -3,6 +3,6 @@ var libnotify = require( '../lib/libnotify.js' ) ;
 
 libnotify.createNotification( {
 	summary: 'Hello world!' ,
-	body: 'This is a <i>Hello world</i> sample code. <b>Thanks for your attention...</b><a href="http://google.com">google</a>' ,
+	body: 'This is a <i>Hello world</i> sample code. <b>Thanks for your attention...</b>' ,
 	iconPath: 'appointment-new' ,
 } ).push() ;
